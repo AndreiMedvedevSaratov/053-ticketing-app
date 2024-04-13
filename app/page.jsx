69758540b7cf1eb2afdfa3d3e5de409f68a2aefd
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2024-04-13 22:54:09 +0400

    Temp results

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,11 @@
+import TicketCard from "./(components)/TicketCard";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return <div>
+    <TicketCard />
+    <TicketCard />
+    <TicketCard />
+  </div>;
 };
 
 export default Dashboard;
